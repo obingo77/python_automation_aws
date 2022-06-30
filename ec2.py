94d2a2e1f288bb7d760d6ab7660378d620116d3a
obingo77 <56453225+obingo77@users.noreply.github.com>
2022-06-30 09:37:04 +0100

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -8,6 +8,6 @@ ec2 = aws_con.client(service_name='ec2',region_name='us-east-1')
 response = ec2.describe_instances()['Reservations']
 
 for i in response:
-    for i in i['Instances']:
+    for i in ['Instances']:
         print("My image id is:{}\nInstance id:{}\nThe launch time is:{}".format(i['ImageId'],ii['InstanceId'],i['LaunchTime'].strftime("%Y-%m-%d %H:%M:%S")))
 
